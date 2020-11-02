95ffb14584963cf946cf233d1543ca23a36d47b1
Leonardo D'Alessandro <leonardo.dalessandro.dev@gmail.com>
2020-11-01 22:34:23 -0300

    same react structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,47 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+//import Home from '../views/Home.vue'
+
+import Landing from '../views/Landing.vue'
+import OrphanagesMap from '../views/OrphanagesMap.vue'
+import CreateOrphanage from '../views/CreateOrphanage.vue'
+import Orphanage from '../views/Orphanage.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // },
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    component: Landing
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/app',
+    name: 'map',
+    component: OrphanagesMap
+  },
+  {
+    path: '/orphanages/create',
+    name: 'create',
+    component: CreateOrphanage
+  },
+  {
+    path: '/orphanages/:id',
+    name: 'show',
+    component: Orphanage
   }
 ]
 
